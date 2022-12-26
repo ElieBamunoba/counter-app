cf0000b4ff4e7cec4239f11e741f70a25bed51cb
ElieBamunoba <eliebamunoba@gmail.com>
2022-12-26 11:41:49 +0300

    testing

--- lib/cubit/counter_state.dart
+++ lib/cubit/counter_state.dart
@@ -1,11 +1,18 @@
 part of 'counter_cubit.dart';
 
 //This class is the blueprint for all possible states which will be emitted by the cubit
-class CounterState {
+class CounterState extends Equatable {
   int counterValue;
   bool? wasIncremented;
   CounterState({
     required this.counterValue,
     this.wasIncremented,
   });
+
+  @override
+  // TODO: implement props
+  List<Object?> get props => [
+        this.counterValue,
+        this.wasIncremented,
+      ];
 }
---
+++ test/cubit/counter_cubit_test.dart
@@ -0,0 +1,48 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:counter_app/cubit/counter_cubit.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group(
+    'CounterCubit',
+    () {
+      CounterCubit counterCubit = CounterCubit();
+      //setUp is the function used to initialize data that the test will work with
+      setUp(() {
+        counterCubit = CounterCubit();
+      });
+      tearDown(() {
+        counterCubit.close();
+      });
+
+      test(
+        'the initial state for the counterCubit is CounterState(CounterValue:0)',
+        () {
+          expect(
+            //the return actual value return by the initial state
+            counterCubit.state,
+            //the expected value to be returned
+            CounterState(counterValue: 0),
+          );
+        },
+      );
+      blocTest(
+        'the cubit shout emit a counterState(counterValue:1, wasIncremented:true) when cubit.increment() function is called',
+        build: () =>
+            counterCubit, //returns the current instance of the CounterCubit in order to make it available to the testing process
+        act: (cubit) => cubit
+            .increment(), //takes the cubit and returns the action perform on it
+
+        expect: () => [CounterState(counterValue: 1, wasIncremented: true)],
+      );
+      blocTest(
+        'the cubit shout emit a counterState(counterValue:-1, wasIncremented:false) when cubit.decrement() function is called',
+        build: () =>
+            counterCubit, //returns the current instance of the CounterCubit in order to make it available to the testing process
+        act: (cubit) => cubit
+            .decrement(), //takes the cubit and returns the action perform on it
+        expect: () => [CounterState(counterValue: -1, wasIncremented: false)],
+      );
+    },
+  );
+}
